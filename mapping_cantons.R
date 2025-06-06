f14361c4cf3952e8a40eedcdc2f38590d94cc8b2
ambiddisco <ambiddisco@gmail.com>
2025-06-06 16:37:43 +0200

    added parameter names for documentation purposes

--- mapping_cantons.R
+++ mapping_cantons.R
@@ -4,6 +4,20 @@ library(dplyr)
 library(tidyr)
 library(viridis)
 
+#' plot_cantons is a function that plots
+#'
+#' @param canton_path a string value, defines path to canton data
+#' @param lake_path a string value, defines path to lake data
+#' @param value_df a tibble, determines the dataframeof values to plot
+#' @param map_crs an integer value, determines the reference system of the
+#' geographical data. Basic value is 2056.
+#' @param value_colname a string value, determines the name of the column plotted
+#' @param title a string value, determines the overall title of the plot
+#'
+#' @returns Nothing, directly plots a map of cantons
+#' @export
+#'
+#' @examples
 plot_cantons <- function(
     canton_path,
     lake_path = NULL,
@@ -12,23 +26,23 @@ plot_cantons <- function(
     value_colname = "avg",
     title = "Map"
 ) {
-  
+
   cantons <- sf::st_read(canton_path, quiet = TRUE) |> sf::st_transform(map_crs)
   lakes <- if (!is.null(lake_path)) sf::st_read(lake_path, quiet = TRUE) |> sf::st_transform(map_crs) else NULL
-  
-  value_df <- value_df |> 
+
+  value_df <- value_df |>
     dplyr::rename(name = canton) |>
     dplyr::mutate(name = as.character(name))
-  
+
   cantons <- dplyr::left_join(cantons, value_df, by = "name")
-  
+
   p <- ggplot2::ggplot() +
     ggplot2::geom_sf(data = cantons, ggplot2::aes(fill = .data[[value_colname]]), color = "black", size = 0.5)
-  
+
   if (!is.null(lakes)) {
     p <- p + ggplot2::geom_sf(data = lakes, fill = "lightblue", color = NA)
   }
-  
+
   p <- p +
     scale_fill_viridis_c(option = "plasma", na.value = "grey90", name = value_colname)
     ggplot2::ggtitle(paste(title, value_colname)) +
@@ -40,7 +54,7 @@ plot_cantons <- function(
       axis.ticks = ggplot2::element_blank(),
       panel.grid = ggplot2::element_blank()
     )
-  
+
   print(p)
 }
 
