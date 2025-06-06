6b025d019dd3e92180a1e446bda2e4fbf613242c
tommaso.pioda <tommaso.pioda@student.supsi.ch>
2025-06-06 10:31:13 +0200

    merged with main project

---
+++ Hackaton_Pioda.R
@@ -0,0 +1,4 @@
+
+
+generic_API_url='https://pollen.googleapis.com/v1/forecast:lookup?key=AIzaSyC92T2AOV70myeLieFg8m2iQS4vaCUQRQg&location.longitude=LONGITUDE_VALUE&location.latitude=LATITUDE_VALUE&
+days=NUMBER_OF_DAYS'
